7d2b8ead4e0283ee588839b758da8282c52718ef
Wraith <97615989+WR117H@users.noreply.github.com>
2025-04-15 23:42:25 +0330

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -24,10 +24,8 @@ const addressSchema = z.object({
   zipCode: z.string().min(5, { message: "Valid ZIP code is required" }),
   country: z.string().min(2, { message: "Country is required" }),
 });
-const sendTratonConnectUI = useTONConnect();
-
 const handleClick = async () => {
-  tonConnectUI.openModal(); // Trigger wallet connect
+  useTONConnect();
   return;
 }
 const Profile = () => {
